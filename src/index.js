7bf84a71c79d8ee866a0819912095fca42f4172e
nox911 <nox.911@gmail.com>
2017-09-29 12:02:20 +0300

    solution with replacing == to ===

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ if (str.length%2) {
 var temp = str.split('');
 for ( var i=1;i<temp.length;i++) {
 	for (var j=0;j<bracketsConfig.length;j++){
-		if ((temp[i] == bracketsConfig[j][1]) && (temp[i-1] == bracketsConfig[j][0])) {
+		if ((temp[i] === bracketsConfig[j][1]) && (temp[i-1] === bracketsConfig[j][0])) {
 			temp.splice(i-1,2);
 			i-=2;
 		}		
